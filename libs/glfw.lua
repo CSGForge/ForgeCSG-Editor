4d49da9bd48a3b66469047f23bc8ab9b0791ba78
JarrodDoyle <thejarroddoyle@gmail.com>
2022-01-20 16:05:06 +0000

    Fixed glfw premake file

--- libs/glfw.lua
+++ libs/glfw.lua
@@ -3,8 +3,28 @@ project "glfw"
     language "C"
 
     includedirs {"glfw/include"}
-    files {"glfw/include/GLFW/*.h", "glfw//src/context.c", "glfw/src/egl_context.*", "glfw/src/init.c", "glfw/src/input.c", "glfw/src/internal.h", "glfw/src/monitor.c", "glfw/src/osmesa_context.*", "glfw/src/vulkan.c", "glfw/src/window.c"}
+    files {
+        "glfw/include/GLFW/*.h",
+        "glfw/src/context.c",
+        "glfw/src/egl_context.*",
+        "glfw/src/init.c",
+        "glfw/src/input.c",
+        "glfw/src/internal.h",
+        "glfw/src/monitor.c",
+        "glfw/src/mappings.h",
+        "glfw/src/null*.*",
+        "glfw/src/osmesa_context.*",
+        "glfw/src/platform.*",
+        "glfw/src/vulkan.c",
+        "glfw/src/window.c"
+    }
 
     filter "system:linux"
         defines "_GLFW_X11"
-        files {"glfw/src/glx_context.*", "glfw/src/linux*.*", "glfw/src/posix*.*", "glfw/src/x11*.*", "glfw/src/xkb*.*"}
+        files {
+            "glfw/src/glx_context.*",
+            "glfw/src/linux*.*",
+            "glfw/src/posix*.*",
+            "glfw/src/x11*.*",
+            "glfw/src/xkb*.*"
+        }
